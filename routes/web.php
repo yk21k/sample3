98210ee83ccffac1dad4eb706e0e23e7d4cca023
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-13 10:02:21 +0900

    AdminController2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminControllers;
+use App\Http\Controllers\Admin\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
