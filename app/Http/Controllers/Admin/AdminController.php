cb9bfd7a0e7b9090b85fcd519c10bece1d8a70ea
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-13 09:55:31 +0900

    AdminContorller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function dashboard(){
+        return view('admin.dashboard');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\AdminControllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/admin')->group(function(){
+    Route::get('dashboard', [AdminController::class, 'dashboard']);
 });
