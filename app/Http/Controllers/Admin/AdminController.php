0f2acddd63bef1061cddb8870e0e8809d09ee0b6
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-13 19:13:13 +0900

    echo_login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            echo "<pre>"; print_r($data); die;
+        }
         return view('admin.login');
     }
 
