a65876efa08245c79ab2a7dd4c94a94981083151
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-14 20:07:45 +0900

    required

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use Validator;
 
 class AdminController extends Controller
 {
@@ -12,6 +13,21 @@ class AdminController extends Controller
         if($request->isMethod('post')){
             $data = $request->all();
             // echo "<pre>"; print_r($data); die;
+
+
+            $rules = [
+                'email' => 'required|email|max:255',
+                'password' => 'required|max:30'
+            ];
+
+            $customMessages = [
+                'email.required' => "Email is required !!",
+                'email.email' => 'Valid Email is required !!',
+                'password.required' => 'Password is required !!'
+            ];
+
+            $this->validate($request, $rules, $customMessages);
+
             if(Auth::guard('admin')->attempt(['email'=>$data['email'], 'password'=>$data['password']])){
                 return redirect("admin/dashboard");
             }else{
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -23,10 +23,28 @@
     </div>
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
-
+      @if($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>
+                        {{ $error }}
+                      </li>
+                  @endforeach
+              </ul>
+          </div>
+      @endif
+      @if(Session::has('error_message'))
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Error:</strong> {{ Session::get('error_message') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      @endif
       <form action="{{ url('admin/login') }}" method="post">@csrf
         <div class="input-group mb-3">
-          <input name="email" type="email" class="form-control" placeholder="Email">
+          <input name="email" type="email" class="form-control" placeholder="Email" required="">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -34,7 +52,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input name="password" type="password" class="form-control" placeholder="Password">
+          <input name="password" type="password" class="form-control" placeholder="Password" required="">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
