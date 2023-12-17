9e317b89b2ad3f51053e0573782f2bfeb1fc79d6
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-17 20:33:14 +0900

    cms_pages_table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AdminsTableSeeder::class);
+        // $this->call(AdminsTableSeeder::class);
+        $this->call(CmsPageTableSeeder::class);
     }
 }
