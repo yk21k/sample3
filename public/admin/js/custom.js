ae2b2cd43f70179c894caa50de40c2016bf753cb
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2023-12-22 21:52:59 +0900

    customCMS

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -56,30 +56,5 @@ $(document).ready(function(){
 	// 	return false;
 	// });
 
-	// Confirm Deletion with SweetAlert
-	$(document).on("click", ".confirmDelete", function(){
-		var record = $(this).attr('record');
-		var recordid = $(this).attr('recordid');
-
-		Swal.fire({
-		  title: "Are you sure?",
-		  text: "You won't be able to revert this!",
-		  icon: "warning",
-		  showCancelButton: true,
-		  confirmButtonColor: "#3085d6",
-		  cancelButtonColor: "#d33",
-		  confirmButtonText: "Yes, delete it!"
-		}).then((result) => {
-		  if (result.isConfirmed) {
-		    Swal.fire(
-		      "Deleted!",
-		      "Your file has been deleted.",
-		      "success"
-		    )
-		    windows.location.href = "/admin/delete-"+record+"/"+recordid;
-		  }
-		});
-
-	});
 
 });
