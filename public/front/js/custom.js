47b3a84663c7a3f25db90706e4c43245dad48db9
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-01-19 17:33:02 +0900

    echodetail

--- public/front/js/custom.js
+++ public/front/js/custom.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 			data:{size:size, product_id:product_id},
 			type:'post',
 			success:function(resp){
-					// alert(resp);
+					alert(resp);
 					if(resp['discount']>0){
 						$(".getAttributePrice").html("<span class='pd-detail__price'>₹"+resp['final_price']+"</span><span class='pd-detail__discount'>("+resp['discount_percent']+"% OFF)</span><del class='pd-detail__del'>₹"+resp['product_price']+"</del>");
 					}else{
