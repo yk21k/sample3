5f7b3b1ed8946312371b33170b7d61e2c2923954
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-02-14 22:31:52 +0900

    priploliindex

--- resources/views/front/privacy_policy.blade.php
+++ resources/views/front/privacy_policy.blade.php
@@ -2,30 +2,61 @@
 .content_body_pp{
 	padding: 20px;
 	background-color: lightgrey;
+	scroll-behavior: smooth;
 }
 
 .content_body_pp p{
 	color: #555555;
 }
 
+.policy_nav1{
+	;
+
+}
+
+.scroll-page_1{
+	;
+
+}
+
 </style>
 
+<!-- Page  -->
+<!-- nav -->
+<nav class="policy_nav1">
+	<h4>Privacy Policy Index</h4>
+	<ul>
+		<a href="#scroll-page_1">Index</a><br>
+		<a href="#scroll-page_2">Article 1 (Personal information)</a><br>
+		<a href="#scroll-page_3">Article 2 (Method of collecting personal information)</a><br>
+		<a href="#scroll-page_4">Article 3 (Purpose of collecting and using personal information)</li><br>
+		<a href="#scroll-page_5">Article 4 (Change of purpose of use)</li><br>
+		<a href="#scroll-page_6">Article 5 (Provision of personal information to third parties)</li><br>
+		<a href="#scroll-page_7">Article 6 (Disclosure of personal information)</li><br>
+		<a href="#scroll-page_8">Article 7 (Correction and deletion of personal information)</li><br>
+		<a href="#scroll-page_9">Article 8 (Suspension of use of personal information, etc.)</li><br>
+		<a href="#scroll-page_10">Article 9 (Changes to privacy policy)</li><br>
+		<a href="#scroll-page_11">Article 10 (Inquiries)</a>
+	</ul>
+</nav>
+<!-- end nav -->
+<!-- body -->
 <div class="content_body_pp">
-	<div><h1>Privacy Policy</h1></div><br><br>
+	<div class="scroll-page_1" id="scroll-page_1"><h1>Privacy Policy</h1></div><br><br>
 	<div><h3>＿＿＿＿＿＿＿＿ (hereinafter referred to as ''our company'') has established the following privacy policy (hereinafter referred to as ''this company'') regarding the handling of users' personal information in the services provided on this website (hereinafter referred to as ''this service''). (hereinafter referred to as "this policy").</h3></div><br>
 	<div>
 		<ul>
-			<div><p><h2>Article 1 (Personal information)</h2></p></div>
+			<div class="scroll-page_2" id="scroll-page_2"><p><h2>Article 1 (Personal information)</h2></p></div>
 			<div>
 				<p><h3>1."Personal information" refers to "personal information" as defined in the Personal Information Protection Act, and is information about a living individual, including name, date of birth, address, telephone number, contact information, etc. This refers to information that can identify a specific individual through descriptions, etc., and information that can identify a specific individual from such information alone, such as data related to physical appearance, fingerprints, voice prints, and the insurer number on a health insurance card (personal identification information).</h3></p>
 			</div>
 			<br><br>	
-			<div><p><h2>Article 2 (Method of collecting personal information)</h2></p></div>
+			<div class="scroll-page_3" id="scroll-page_3"><p><h2>Article 2 (Method of collecting personal information)</h2></p></div>
 			<div>
 				<p><h3>1.When a user registers for use, we may ask for personal information such as name, date of birth, address, telephone number, email address, bank account number, credit card number, driver's license number, etc. In addition, information regarding transaction records and payments, including the user's personal information, made between users and affiliates, etc., including the user's personal information, may be used by our affiliates (including information providers, advertisers, advertising distribution destinations, etc., hereinafter referred to as " (hereinafter referred to as "partners"), etc.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 3 (Purpose of collecting and using personal information)</h2></p></div>
+			<div class="scroll-page_4" id="scroll-page_4"><p><h2>Article 3 (Purpose of collecting and using personal information)</h2></p></div>
 			<div>
 				<p><h3>1.The purposes for which our company collects and uses personal information are as follows.</h3></p>
 				<ul>
@@ -40,13 +71,13 @@
 				</ul>
 			</div>
 			<br><br>
-			<div><p><h2>Article 4 (Change of purpose of use)</h2></p></div>
+			<div class="scroll-page_5" id="scroll-page_5"><p><h2>Article 4 (Change of purpose of use)</h2></p></div>
 			<div>
 				<p><h3>1.Our company will change the purpose of use of personal information only if it is reasonably recognized that the purpose of use is related to the purpose before the change.</h3></p>
 				<p><h3>2.If the purpose of use is changed, the changed purpose will be notified to users or announced on this website by the method prescribed by the Company.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 5 (Provision of personal information to third parties)</h2></p></div>
+			<div class="scroll-page_6" id="scroll-page_6"><p><h2>Article 5 (Provision of personal information to third parties)</h2></p></div>
 			<div>
 				<p><h3>1.Our company will not provide personal information to third parties without obtaining the user's consent in advance, except in the following cases. However, this excludes cases permitted by the Personal Information Protection Act and other laws and regulations.</h3></p>
 				<ul>
@@ -68,7 +99,7 @@
 				</ul>
 			</div>
 			<br><br>
-			<div><p><h2>Article 6 (Disclosure of personal information)</h2></p></div>
+			<div class="scroll-page_7" id="scroll-page_7"><p><h2>Article 6 (Disclosure of personal information)</h2></p></div>
 			<div>
 				<p><h3>1.When we are requested by a person to disclose personal information, we will disclose this to the person without delay. However, if disclosure falls under any of the following, we may not disclose all or part of the information, and if we decide not to disclose it, we will notify you to that effect without delay. Please note that a fee of 1,000 yen will be charged for each disclosure of personal information.</h3></p>
 				<ul>
@@ -81,7 +112,7 @@
 				<p><h3>2.If the purpose of use is changed, the changed purpose will be notified to users or announced on this website by the method prescribed by the Company.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 7 (Correction and deletion of personal information)</h2></p></div>
+			<div class="scroll-page_8" id="scroll-page_8"><p><h2>Article 7 (Correction and deletion of personal information)</h2></p></div>
 			<div>
 				<p><h3>1.If the user's personal information held by the Company is incorrect, the user may contact the Company to correct, add, or delete the personal information (hereinafter referred to as "Correction, etc.") according to the procedures established by the Company. ) can be claimed.</h3></p>
 				
@@ -90,7 +121,7 @@
 				<p><h3>3.If the Company makes any corrections, etc. based on the provisions of the preceding paragraph, or if it decides not to make any corrections, etc., the Company will notify the User without delay.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 8 (Suspension of use of personal information, etc.)</h2></p></div>
+			<div class="scroll-page_9" id="scroll-page_9"><p><h2>Article 8 (Suspension of use of personal information, etc.)</h2></p></div>
 			<div>
 				<p><h3>1.Our company may suspend or erase the use of personal information (hereinafter referred to as ``Use of Personal Information'') due to the reason that the personal information is being handled beyond the scope of the purpose of use or that it was obtained by illegal means. If we are requested to do so, we will conduct the necessary investigation without delay.</h3></p>
 				<p><h3>2.If we determine that it is necessary to respond to the request based on the results of the investigation in the preceding paragraph, we will suspend the use of the personal information without delay.</h3></p>
@@ -98,13 +129,13 @@
 				<p><h3>4.Notwithstanding the preceding two paragraphs, in cases where suspension of use, etc. would require a large amount of cost, or where suspension of use, etc. is difficult, alternative measures necessary to protect the rights and interests of the user may be taken. In such cases, this alternative measure shall be taken.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 9 (Changes to privacy policy)</h2></p></div>
+			<div class="scroll-page_10" id="scroll-page_10"><p><h2>Article 9 (Changes to privacy policy)</h2></p></div>
 			<div>
 				<p><h3>1.The contents of this policy may be changed without notice to users, except for laws and other matters otherwise specified in this policy.</h3></p>
 				<p><h3>2.Unless otherwise specified by our company, the revised privacy policy will become effective from the time it is posted on this website.</h3></p>
 			</div>
 			<br><br>
-			<div><p><h2>Article 10 (Inquiries)</h2></p></div>
+			<div class="scroll-page_11" id="scroll-page_11"><p><h2>Article 10 (Inquiries)</h2></p></div>
 			<div>
 				<p><h3>For inquiries regarding this policy, please contact the contact point below.</h3></p>
 				<ul>
@@ -151,3 +182,10 @@
 		</ul>
 	</div> -->
 </div>
+<!-- end body -->
+<!--  end Page -->
+
+
+
+
+
