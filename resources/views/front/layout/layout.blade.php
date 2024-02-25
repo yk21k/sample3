fb62c74d08f026413a8d860071e2fa6e857149df
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-02-25 19:27:12 +0900

    javascripterror

--- resources/views/front/layout/layout.blade.php
+++ resources/views/front/layout/layout.blade.php
@@ -82,7 +82,22 @@
         </div>
         
         <!--====== End - Main App ======-->
-        
+
+        <!--====== Noscript ======-->
+        <noscript>
+            <div class="app-setting">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="app-setting__wrap">
+                                <h1 class="app-setting__h1">JavaScript is disabled in your browser.</h1>
+                                <span class="app-setting__text">Please enable JavaScript in your browser or upgrade to a JavaScript-capable browser.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </noscript>
         <!--====== Google Analytics: change UA-XXXXX-Y to be your site's ID ======-->
         <script>
             window.ga = function() {
@@ -115,21 +130,5 @@
 
         <!-- Handling of personal information -->
         <script src="{{ url('front/js/personal_info.js') }}"></script>
-
-        <!--====== Noscript ======-->
-        <noscript>
-            <div class="app-setting">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="app-setting__wrap">
-                                <h1 class="app-setting__h1">JavaScript is disabled in your browser.</h1>
-                                <span class="app-setting__text">Please enable JavaScript in your browser or upgrade to a JavaScript-capable browser.</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </noscript>
     </body>
 </html>
