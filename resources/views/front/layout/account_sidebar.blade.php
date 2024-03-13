7e927d9d79210a0c2554328dbe0439c73379d8f1
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-03-13 15:11:05 +0900

    order23

--- resources/views/front/layout/account_sidebar.blade.php
+++ resources/views/front/layout/account_sidebar.blade.php
@@ -5,7 +5,7 @@
 <!--====== Dashboard Features ======-->
 <div class="dash__box dash__box--bg-white dash__box--shadow u-s-m-b-30">
     <div class="dash__pad-1">
-        <span class="dash__text u-s-m-b-16">Hello, <strong>{{ $user_name['name'] }} {{ $orderDetails['name'] }} {{ $orderDetails['user']['name'] }}</strong> </span>
+        <span class="dash__text u-s-m-b-16">Hello, <strong>{{ $user_name['name'] }} </strong> </span>
         <ul class="dash__f-list">
             <li><a href="{{ url('user/account') }}">My Billing/Contact Address</a></li>
             <li><a href="{{ url('user/orders') }}">My Orders</a></li>
