1e9de8dadb685bf15701e3f39e68d92c2a1f0546
軽石恭平 <kyouk@karuishikyouheinoMacBook-Air.local>
2024-04-10 21:04:25 +0900

    courier fu

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\OrderStatus;
 use App\Models\OrdersLog;
 use Session;
+use Auth;
+
 
 class OrdersController extends Controller
 {
@@ -37,6 +41,26 @@ class OrdersController extends Controller
             // Update Courier Name & Tracking Number
             if(!empty($data['courier_name'])&&!empty($data['tracking_number'])){
                 Order::where('id', $data['order_id'])->update(['courier_name'=>$data['courier_name'], 'tracking_number'=>$data['tracking_number']]);
+
+                // Send Order Shipped Email to Customer with Tracking Details
+
+                $orderDetails = Order::with('orders_products', 'user')->where('id', $data['order_id'])->first()->toArray(); //*
+                // echo "Order Done"; die;
+
+                // Send Order Shipped Email
+                $email = $orderDetails['user']['email']; //*
+                $messageData = [
+                    'email' => $email,
+                    'name' => $orderDetails['user']['email'], //*
+                    'order_id' => $data['order_id'], //*
+                    'order_status' => $data['order_status'],
+                    'courier_name' => $data['courier_name'],
+                    'tracking_number' => $data['tracking_number'],
+                    'orderDetails' => $orderDetails
+                ];
+                Mail::send('emails.shipped_order', $messageData, function($message)use($email){
+                    $message->to($email)->subject('Order Shipped (Tracking Details) - Sample3');
+                });
             }
 
             // Insert Order Status in Order Logs
@@ -44,6 +68,8 @@ class OrdersController extends Controller
             $log->order_id = $data['order_id'];
             $log->order_status = $data['order_status'];
             $log->save();
+
+
 
             $message = "Order Status has been updated Successfully!!";
             return redirect()->back()->with('success_message', $message);
